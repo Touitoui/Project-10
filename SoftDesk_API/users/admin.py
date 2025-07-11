f58b14d1951bba97a7f8a7cac16da7709cdb3e98
Mickael <mickael.gresse@gmail.com>
2025-07-11 09:18:11 +0200

    Update admin.py

--- SoftDesk_API/users/admin.py
+++ SoftDesk_API/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User, Contributor, Project, Issue, Comment
+from project.models import Project, Issue, Comment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Contributor)
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Comment)
