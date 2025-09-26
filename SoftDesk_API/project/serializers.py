71e8a28058d31344d1cf67f6aad79e565eb845fa
Mickael <mickael.gresse@gmail.com>
2025-09-26 12:39:34 +0200

    fixed comments and issues serializer

--- SoftDesk_API/project/serializers.py
+++ SoftDesk_API/project/serializers.py
@@ -1,11 +1,10 @@
-from rest_framework.serializers import ModelSerializer, ValidationError
-from datetime import date
+from rest_framework.serializers import ModelSerializer, ValidationError, StringRelatedField, HiddenField
 from project.models import Project, Issue, Comment
  
 class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'type', 'created_time']
+        fields = ['id', 'name', 'description', 'type', 'created_time', 'author']
         extra_kwargs = {'author': {'read_only': True}}
 
     def validate_name(self, value):
@@ -14,42 +13,21 @@ class ProjectSerializer(ModelSerializer):
             raise ValidationError("Name must be alphanumeric.")
         return value
 
-    # def validate_type(self, value):
-    #     print(f"Validating type: {value} - {value not in dict(Project.TYPE_CHOICES).keys()}")
-    #     if value not in dict(Project.TYPE_CHOICES).keys():
-    #         raise ValidationError("Type must be one of: " + ", ".join(dict(Project.TYPE_CHOICES).keys()))
-    #     return value
-
-    # def validate_description(self, value):
-    #     if not value:
-    #         raise ValidationError("Description is required.")
-    #     return value
-
 
 class IssueSerializer(ModelSerializer):
- 
+    project_info = ProjectSerializer(read_only=True, source='project')
+
     class Meta:
         model = Issue
-        fields = ['id', 'title', 'description', 'status', 'project']
-
-    def validate_title(self, value):
-        if not value:
-            raise ValidationError("Title is required.")
-        return value
-
-    def validate_description(self, value):
-        if not value:
-            raise ValidationError("Description is required.")
-        return value
+        fields = ['id', 'title', 'description', 'status', 'project', "project_info", "status", 'priority', "tag", 'author', 'created_time']
+        extra_kwargs = {'author': {'read_only': True}}
 
 
 class CommentSerializer(ModelSerializer):
- 
+    issue_info = IssueSerializer(read_only=True, source='issue')
+
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'issue', 'user']
+        fields = ['id', 'description', 'issue', 'issue_info', 'created_time']
+        extra_kwargs = {'author': {'read_only': True}}
 
-    def validate_content(self, value):
-        if not value:
-            raise ValidationError("Content is required.")
-        return value
